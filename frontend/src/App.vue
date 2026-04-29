1e9d0612b7789733df6e6ea62df82ab53293200d
Alessandro Kopani <alessandrokopan03@gmail.com>
2026-04-29 23:38:09 +0200

    Creazione css globale e modifiche grafiche

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -14,18 +14,3 @@ import AppFooter from './components/AppFooter.vue'
     <AppFooter />
   </div>
 </template>
-
-<style scoped>
-.app-layout {
-  min-height: 100vh;
-  display: flex;
-  flex-direction: column;
-}
-
-.page-content {
-  width: 100%;
-  max-width: 1100px;
-  margin: 0 auto;
-  padding: 32px 24px;
-}
-</style>
