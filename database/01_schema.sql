5b4b2e6db2232764490e6b821bfb4aa1aa634861
Xuang Lu <luxng2102@gmail.com>
2026-05-07 17:07:03 +0200

    Aggiunta tabelle ponte prodotti

--- database/01_schema.sql
+++ database/01_schema.sql
@@ -2,6 +2,8 @@
 -- Tabelle iniziali:
 -- users, supermarkets, categories, products, allergens
 
+DROP TABLE IF EXISTS product_allergens;
+DROP TABLE IF EXISTS supermarket_products;
 DROP TABLE IF EXISTS products;
 DROP TABLE IF EXISTS categories;
 DROP TABLE IF EXISTS allergens;
@@ -91,6 +93,24 @@ CREATE TABLE products (
     )
 );
 
+-- Tabella supermarket_products
+-- Collega prodotti e supermercati
+-- Gestisce disponibilità e stock per ogni punto vendita
+CREATE TABLE supermarket_products (
+    supermarket_id BIGINT REFERENCES supermarkets(id) ON DELETE CASCADE,
+    product_id BIGINT REFERENCES products(id) ON DELETE CASCADE,
+
+    stock_quantity INTEGER DEFAULT 0 CHECK (stock_quantity >= 0),
+    is_available BOOLEAN DEFAULT TRUE,
+
+    local_price NUMERIC(10,2) CHECK (local_price >= 0),
+
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+
+    PRIMARY KEY (supermarket_id, product_id)
+);
+
 -- Tabella allergens
 -- Salva gli allergeni standard
 CREATE TABLE allergens (
@@ -102,4 +122,13 @@ CREATE TABLE allergens (
 
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+-- Tabella product_allergens
+-- Collega prodotti e allergeni
+CREATE TABLE product_allergens (
+    product_id BIGINT REFERENCES products(id) ON DELETE CASCADE,
+    allergen_id BIGINT REFERENCES allergens(id) ON DELETE CASCADE,
+
+    PRIMARY KEY (product_id, allergen_id)
 );
