51602e67761378d63b2a74c7ace9d5e38338cbd6
Alessandro Kopani <alessandrokopan03@gmail.com>
2026-05-09 19:03:08 +0200

    Implementazione API categorie e allergeni

--- backend/controllers/allergenController.js
+++ backend/controllers/allergenController.js
@@ -1,7 +1,21 @@
-function getAllergens(req, res) {
-  res.json({
-    message: 'Lista allergeni da implementare',
-  })
+const pool = require('../db')
+
+async function getAllergens(req, res, next) {
+  try {
+    const result = await pool.query(`
+      SELECT
+        id,
+        name,
+        label,
+        description
+      FROM allergens
+      ORDER BY id
+    `)
+
+    res.json(result.rows)
+  } catch (error) {
+    next(error)
+  }
 }
 
 module.exports = {
--- backend/controllers/categoryController.js
+++ backend/controllers/categoryController.js
@@ -1,7 +1,21 @@
-function getCategories(req, res) {
-  res.json({
-    message: 'Lista categorie da implementare',
-  })
+const pool = require('../db')
+
+async function getCategories(req, res, next) {
+  try {
+    const result = await pool.query(`
+      SELECT
+        id,
+        name,
+        description,
+        parent_id
+      FROM categories
+      ORDER BY id
+    `)
+
+    res.json(result.rows)
+  } catch (error) {
+    next(error)
+  }
 }
 
 module.exports = {
